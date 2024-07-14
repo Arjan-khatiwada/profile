86149cfea353810c6a6712acde196672cf6610ed
Arjan-khatiwada <145360397+Arjan-khatiwada@users.noreply.github.com>
2024-07-14 09:13:47 +0545

    Add files via upload

---
+++ about_me.php
@@ -0,0 +1,160 @@
+<?php
+$title = "About Me";
+
+include("header.php");
+?>
+    <main>
+       <div class="container" style="color:white;">
+            <div>
+                <img src="images/profilebg.jpg" alt=""  class="position-absolute  top-5" style="width:90%; height: 50%;">
+            </div>
+            <div class="row pt-5 px-5">
+                <div class="col-md-8">
+                    <div class="card">
+                       <div class="card-body">
+                            <p class="text-uppercase text-sm">My Information</p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Date of Birth</label>
+                                        <input class="form-control" type="text" value="2003-8-16 AD(2060-4-31 BS) " onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Nationality</label>
+                                        <input class="form-control" type="email" value="Nepali" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                           </div>
+                            
+                            <hr class="horizontal  " >
+                            <p class="text-uppercase text-sm">Contact Information</p>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Address</label>
+                                        <input class="form-control" type="text" value="Budhiganga-1 puspalalchowk" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">City</label>
+                                        <input class="form-control" type="text" value="Biratnagar" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Pardesh</label>
+                                        <input class="form-control" type="text" value="Koshi" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="horizontal dark">
+                            <p class="text-uppercase text-sm">Skilled</p>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">HTML/CSS</label>
+                                        <input class="" type="range" value="70" min="0"max="100" onfocus="focused(this)" onfocusout="defocused(this)" style="display: block; width: 100%; padding: .375rem .75rem;"  disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">C Language</label>
+                                        <input class="" type="range" value="50" min="0"max="100" onfocus="focused(this)" onfocusout="defocused(this)"style="display: block; width: 100%; padding: .375rem .75rem;" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Java</label>
+                                        <input class="" type="range" value="65" min="0"max="100" onfocus="focused(this)" style="display: block; width: 100%; padding: .375rem .75rem;"  onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Taly</label>
+                                        <input class="" type="range" value="40" min="0"max="100" onfocus="focused(this)" style="display: block; width: 100%; padding: .375rem .75rem;"  onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">PHP/Javascript</label>
+                                        <input class="" type="range" value="70" min="0"max="100" onfocus="focused(this)" style="display: block; width: 100%; padding: .375rem .75rem;"  onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">My SQL</label>
+                                        <input class="" type="range" value="45" min="0"max="100" onfocus="focused(this)" style="display: block; width: 100%; padding: .375rem .75rem;"  onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">MS Office</label>
+                                        <input class="" type="range" value="67" min="0"max="100" onfocus="focused(this)" style="display: block; width: 100%; padding: .375rem .75rem;"  onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="horizontal dark">
+                            <p class="text-uppercase text-sm">Language</p>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input class="form-control " type="text" value="Nepali" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" value="English" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" value="Hindi" onfocus="focused(this)" onfocusout="defocused(this)" disabled>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+                 <div class="col-md-4 ">
+                    <div class="card card-profile">
+                            <img src="images/ppbg.jpg" alt="Image placeholder" class="card-img-top">
+                            
+                                
+                            <div class="col-md-4 ">
+                            <img src="images/profile1.png" class="rounded-circle img-fluid border border-2 border-white position-absolute top-50 start-50 translate-middle" style="width:40%; height: 35%; ">
+                            </div>
+
+                        <div class="card-body pt-0 start-60 ">
+                            <div class="start-90"> 
+                                <div class="text-center mt-5 pt-5">
+                                    <h5>
+                                    ARJAN KHATIWADA
+                                   </h5>
+                                   <span class="font-weight-light"> Age: 21</span>
+                                    <div class="h6 font-weight-300">
+                                    <i class="ni location_pin mr-2"></i>Biratnagar, Morang
+                                    </div>
+                                    <div class="h6 mt-4">
+                                    <i class="ni business_briefcase-24 mr-2"></i>Bachelor in Computer Application
+                                    </div>
+                                    <div>
+                                    <i class="ni education_hat mr-2"></i>Faculties Of Humanities and Social Science.
+                                    </div>
+                                </div>
+                            </div>
+                           
+                          
+                      </div>
+                    </div>
+              </div>
+            </div>
+       </div>
+    </main>
+    
+
+<?php
+    include('footer1.php');
+ ?>
